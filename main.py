9ae0133d49042d868e028f504bc0bdc93527ac72
evvgeniy978 <evvgeniy978@gmail.com>
2025-02-04 20:13:42 +0100

    exercise1.1

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-print("Hello World")
-
-print("Hello Git")
+from datetime import datetime
+def get_days_from_today(date: str) -> int:
+    try:
+        arbitrary_date = datetime.strptime(date, "%Y-%m-%d").date()
+        today_date = datetime.today().date()
+        days_difference = (arbitrary_date - today_date).days
+        return days_difference
+        
+    except ValueError:
+            raise ValueError("Invalid date format. Use 'YYYY-MM-DD'.")
+print(get_days_from_today("2022-02-03"))
