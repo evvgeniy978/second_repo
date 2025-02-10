61b72914c0ed9b4a15bfe5902e96d322d7ee833d
evvgeniy978 <evvgeniy978@gmail.com>
2025-02-10 18:57:11 +0100

    exercise_2

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from datetime import datetime
-def get_days_from_today(date: str) -> int:
-    try:
-        arbitrary_date = datetime.strptime(date, "%Y-%m-%d").date()
-        today_date = datetime.today().date()
-        days_difference = (arbitrary_date - today_date).days
-        return days_difference
-        
-    except ValueError:
-            raise ValueError("Invalid date format. Use 'YYYY-MM-DD'.")
-print(get_days_from_today("2022-02-03")) 
+import random
+
+def get_numbers_ticket(min: int, max: int, quantity: int):
+    
+    if not (1 <= min <= max <= 1000):
+        return []
+    if not (1 <= quantity <= (max - min + 1)):  
+        return []
+    
+    numbers = random.sample(range(min, max + 1), quantity)
+    
+    return sorted(numbers)
+
+lottery_numbers = get_numbers_ticket(1, 49, 6)
+print("Your lottery numbers:", lottery_numbers)
